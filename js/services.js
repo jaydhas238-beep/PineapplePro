e6c5dce47c325aec656c692caabcc8f2500bdc6d
jaydhas238-beep <jaydhas238@gmail.com>
2026-07-30 01:09:10 +0530

    Add FAQ toggle functionality

--- js/services.js
+++ js/services.js
@@ -1,1 +1,14 @@
 
+const faqItems = document.querySelectorAll(".faq-item");
+
+faqItems.forEach(item => {
+
+    const button = item.querySelector(".faq-question");
+
+    button.addEventListener("click", () => {
+
+        item.classList.toggle("active");
+
+    });
+
+});
